5b972210eded25131869218082f90820a7a3e637
lmno3418 <lmno3418@gmail.com>
2026-04-09 12:56:41 +0530

    modular level done

--- src/main/Databases/mysqlcon.py
+++ src/main/Databases/mysqlcon.py
@@ -4,34 +4,107 @@ import mysql.connector
 
 from src.main.encryp_decrypt import AES
 
+class MySQLConnection:
+    def __init__(self, config):
+        self.config = config
 
-def read_from_mysql(config,query):
-    try:
-        con = mysql.connector.connect(
-            host=config["MySQL"]["host"],
-            user=AES.decrypt(config["MySQL"]["user"]),       #encrypted value in config.ini, decrypted using AES.py
-            password=AES.decrypt(config["MySQL"]["password"]),       #encrypted value in config.ini, decrypted using AES.py
-            database=AES.decrypt(config["MySQL"]["database"])        #encrypted value in config.ini, decrypted using AES.py
-        )
+    def connect(self):
+        try:
+            self.con = mysql.connector.connect(
+                host=self.config["MySQL"]["host"],
+                user=AES.decrypt(self.config["MySQL"]["user"]),               #encrypted value in config.ini, decrypted using AES.py
+                password=AES.decrypt(self.config["MySQL"]["password"]),       #encrypted value in config.ini, decrypted using AES.py
+                database=AES.decrypt(self.config["MySQL"]["database"])        #encrypted value in config.ini, decrypted using AES.py
+            )
+            logger.info("MySQL connection established successfully.")
+            return self.con
+        
+        except Exception as e: 
+            logger.error(f"Error occurred while connecting to MySQL. Details: {e}")
+            raise e
+        
+    def close(self):
+        try:
+            if hasattr(self, 'con') and self.con.is_connected():
+                self.con.close()
+                logger.info("MySQL connection closed successfully.")
+        except Exception as e:
+            logger.error(f"Error occurred while closing MySQL connection. Details: {e}")
+            raise e    
+        
+        
+class MysqlCrudOperations:
+    def __init__(self, connection):
+        self.connection = connection
+        
+    def read_from_mysql(self, query):
+        try:
+            if self.connection.is_connected():
+                cur = self.connection.cursor()
+                cur.execute(query)
+                result = cur.fetchall()
+                cur.close()
+                return result
+            else:
+                raise Exception("MySQL connection is not established.")
+        except Exception as e:
+            logger.error(f"Error occurred while executing query. Details: {e}")
+            raise e
+        
+    def write_to_mysql(self, query):
+        try:
+            if self.connection.is_connected():
+                cur = self.connection.cursor()
+                cur.execute(query)
+                self.connection.commit()
+                cur.close()
+                logger.info("Data written to MySQL successfully.")
+            else:
+                raise Exception("MySQL connection is not established.")
+        except Exception as e:
+            logger.error(f"Error occurred while executing query. Details: {e}")
+            raise e
+        
+    def update_mysql(self, query):
+        try:
+            if self.connection.is_connected():
+                cur = self.connection.cursor()
+                cur.execute(query)
+                self.connection.commit()
+                cur.close()
+                logger.info("Data updated in MySQL successfully.")
+            else:
+                raise Exception("MySQL connection is not established.")
+        except Exception as e:
+            logger.error(f"Error occurred while executing query. Details: {e}")
+            raise e
+        
+    def delete_from_mysql(self, query):
+        try:
+            if self.connection.is_connected():
+                cur = self.connection.cursor()
+                cur.execute(query)
+                self.connection.commit()
+                cur.close()
+                logger.info("Data deleted from MySQL successfully.")
+            else:
+                raise Exception("MySQL connection is not established.")
+        except Exception as e:
+            logger.error(f"Error occurred while executing query. Details: {e}")
+            raise e
+        
+    def execute_query(self, query):
+        try:
+            if self.connection.is_connected():
+                cur = self.connection.cursor()
+                cur.execute(query)
+                result = cur.fetchall()
+                cur.close()
+                return result
+            else:
+                raise Exception("MySQL connection is not established.")
+        except Exception as e:
+            logger.error(f"Error occurred while executing query. Details: {e}")
+            raise e
+        
 
-
-        # logger.info(f"{con}")       #check connection
-
-        cur = con.cursor()          #creating cursor object to execute SQL queries
-
-        cur.execute(query)         #executing query
-        result = cur.fetchall()                             #fetching all results from the executed query
-
-        # logger.info(result)                                 #logging the results to check if the query was executed successfully
-        return result
-    
-    except Exception as e:
-        logger.error(f"Error occurred in MySQL. Details: {e}")
-        raise e
-    
-    finally:
-        if 'cur' in locals():
-            cur.close()
-        if 'con' in locals():
-            con.close()
-        logger.info("MySQL cursor & connection closed.")
--- src/main/main.py
+++ src/main/main.py
@@ -8,12 +8,20 @@ logger = loguru.logger
 import configparser
 
 config = configparser.ConfigParser()
-config.read('/Users/lmno3418/Documents/PROJECTS/PythonProject/src/resources/config.ini')
+config.read('/Users/lmno3418/Documents/PROJECTS/project1/src/resources/config.ini')
 
 def main():
-    query="select * from labours_table;"
-    result = read_from_mysql(config=config, query=query)         #example of executing a query using the read_from_mysql function
-    logger.info(result)
+    
+    mysql_connection = MySQLConnection(config)
+    connection = mysql_connection.connect()        #establishing connection to MySQL database using the MySQLConnection class
+    
+    crud_operations = MysqlCrudOperations(connection)        #creating an instance of the MysqlCrudOperations class to perform CRUD operations on the MySQL database
+    
+    query = "SELECT * FROM labours_table;"                #example query to fetch all records from the labours_table
+    result = crud_operations.read_from_mysql(query)      #executing the query using the read_from_mysql method of the MysqlCrudOperations class
+    logger.info(result)                                   #logging the result of the query execution
+    
+    mysql_connection.close()                       #closing the MySQL connection 
     
      
 if __name__ == "__main__":
